699a10fa49a400779a2fc8311f6b04dddb8883ce
Gokaraju Purushothama Gopi Krishnam Raju <gopikr@visionplanner.com>
2022-07-06 11:16:22 +0530

    Improvements

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -17,9 +17,6 @@ const Question = ({
   check
 }) => {
   const [selected, setSelected] = useState();
-  const[ques,setQues]=useState("")
-  const[qNo,setQno]=useState(1)
-  const[quesFlag,setQuesFlag]=useState(false)
   const [error, setError] = useState(false);
 
 
@@ -32,7 +29,7 @@ const Question = ({
   };
 
   const handleCheck = (i) => {
-    console.log("info",i)
+    console.log("info", i)
     setSelected(i);
     if (i === correct) setScore(score + 1);
     setError(false);
@@ -41,19 +38,13 @@ const Question = ({
   const handleNext = () => {
     if (currQues >= 4) {
       history.push("/result");
-    } 
-      setCurrQues(currQues + 1);
-      setSelected();
-     
+    }
+    setCurrQues(currQues + 1);
+    setSelected();
+
   };
-  const handleClick=(quesIndex)=>{
-    setCurrQues(quesIndex)
-    setQues(questions[quesIndex].question)
-    setQno(quesIndex+1)
-    
-  }
-  
-  const handlePrev=()=>{
+
+  const handlePrev = () => {
     setCurrQues(currQues - 1);
   }
 
@@ -64,25 +55,25 @@ const Question = ({
 
   return (
     <div className="question">
-   <div className="questions-five">
-   <Button  variant="contained" onClick={() => handleClick(0)}>Question1</Button>
-     <Button   variant="contained"onClick={() => handleClick(1)}>Question2</Button>
-     <Button  variant="contained" onClick={() => handleClick(2)}>Question3</Button>
-     <Button   variant="contained" onClick={() => handleClick(3)}>Question4</Button>
-     <Button variant="contained" onClick={() => handleClick(4)}>Question5</Button>
-   </div>
-    
+      <div className="questions-five">
+        <Button variant="contained" onClick={() => setCurrQues(0)}>Question1</Button>
+        <Button variant="contained" onClick={() => setCurrQues(1)}>Question2</Button>
+        <Button variant="contained" onClick={() => setCurrQues(2)}>Question3</Button>
+        <Button variant="contained" onClick={() => setCurrQues(3)}>Question4</Button>
+        <Button variant="contained" onClick={() => setCurrQues(4)}>Question5</Button>
+      </div>
 
-      { check ? <div className="singleQuestion">
-        <button onClick={() => handleClick(0)}>b1</button>
-      <h1> check Answers { quesFlag ? qNo : currQues + 1} :</h1>
-        <h2>{quesFlag ? ques : questions[currQues].question}</h2>
+
+      {check ? <div className="singleQuestion">
+        <button onClick={() => setCurrQues(0)}>b1</button>
+        <h1> check Answers {currQues + 1} :</h1>
+        <h2>{questions[currQues].question}</h2>
         <div className="options">
           {error && <ErrorMessage>{error}</ErrorMessage>}
           {options &&
             options.map((i) => (
               <button
-              className={`singleOption  ${handleSelect(i)}`}
+                className={`singleOption  ${handleSelect(i)}`}
                 key={i}
                 onClick={() => handleCheck(i)}
                 disabled={true}
@@ -92,10 +83,10 @@ const Question = ({
             ))}
         </div>
         <div className="controls">
-          
+
           <Button
-          color="secondary"
-           onClick={handlePrev}>Previous</Button>
+            color="secondary"
+            onClick={handlePrev}>Previous</Button>
           <Button
             variant="contained"
             color="primary"
@@ -103,58 +94,58 @@ const Question = ({
             style={{ width: 185 }}
             onClick={handleNext}
           >
-            {currQues >=4 ? "close" : "Next Question"}
-            
+            {currQues >= 4 ? "close" : "Next Question"}
+
           </Button>
-        
-         
+
+
         </div>
-      </div> : 
-      
-      
-      <div className="singleQuestion">
+      </div> :
 
-         <h1>Question { quesFlag ? qNo : currQues + 1} :</h1>
-         {console.log("ques",questions, "curr", currQues)}
-        <h2>{questions[currQues].question}</h2>
-        <div className="options">
-          {error && <ErrorMessage>{error}</ErrorMessage>}
-          {options &&
-            options.map((i) => (
-              <Button
 
-                // className={`singleOption  ${selected && handleSelect(i)}`}
-                key={i}
-                onClick={() => handleCheck(i)}
+        <div className="singleQuestion">
+
+          <h1>Question {currQues + 1} :</h1>
+          {console.log("ques", questions, "curr", currQues)}
+          <h2>{questions[currQues].question}</h2>
+          <div className="options">
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            {options &&
+              options.map((i) => (
+                <Button
+
+                  // className={`singleOption  ${selected && handleSelect(i)}`}
+                  key={i}
+                  onClick={() => handleCheck(i)}
                 // disabled={selected}
-              >
-                {i}
-              </Button>
-            ))}
-        </div>
-        <div className="controls">
-          
-          <Button
-         
-          variant="contained"
-          color="primary"
-          size="large"
-          disabled={currQues === 0}
-           onClick={handlePrev}>Previous</Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            style={{ width: 185 }}
-            onClick={handleNext}
-          >
-            {currQues  >=4 ? "Submit" : "Next Question"}
-            
-          </Button>
-        
-         
-        </div>
-      </div>}
+                >
+                  {i}
+                </Button>
+              ))}
+          </div>
+          <div className="controls">
+
+            <Button
+
+              variant="contained"
+              color="primary"
+              size="large"
+              disabled={currQues === 0}
+              onClick={handlePrev}>Previous</Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              style={{ width: 185 }}
+              onClick={handleNext}
+            >
+              {currQues >= 4 ? "Submit" : "Next Question"}
+
+            </Button>
+
+
+          </div>
+        </div>}
     </div>
   );
 };
